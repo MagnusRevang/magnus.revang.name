165ebc6c04f66e656b469315eda5bf5f96b1de79
Magnus Revang <github@revang.name>
2012-03-08 09:44:15 +0100

    Added frame busting script

--- js/site.js
+++ js/site.js
@@ -1,4 +1,9 @@
 $(function () { // onDOMLoaded
 	// change .no-js class on BODY tag to .js to enable js specific styles
 	$(".no-js").removeClass("no-js").addClass("js");
+
+	// break out of frames
+	if (window.self !== window.top) {
+		window.top.location = window.self.location;
+	}
 });
